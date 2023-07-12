0031717cae889f11babb41520d922b101af9c928
ApoloSE <121982692+ApoloSE@users.noreply.github.com>
2023-07-12 16:32:40 -0600

    Delete 3_exercise.py

--- 3_exercise.py
+++
@@ -1,6 +0,0 @@
-# Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética
-# 3 más 2, divido por la multiplicación de 2 por 5, el resultado elevado al cuadrado
-
-r = ((3+2)/(2*5))
-r*=r
-print(r)
